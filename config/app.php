8ffc32cf216d28651b5064e9b9bfa6361297edaa
Asif Raza <asif.raza1013@gmail.com>
2022-10-23 09:49:02 -0700

    hide laravel fcm

--- config/app.php
+++ config/app.php
@@ -162,7 +162,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Overtrue\LaravelFollow\FollowServiceProvider::class,
-        LaravelFCM\FCMServiceProvider::class,
+        // LaravelFCM\FCMServiceProvider::class,
 
         /*
          * Package Service Providers...
